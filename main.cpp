2b258b72f37e55f284b10e3a7199b1225a3cce02
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-02-08 16:44:25 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,18 +2,34 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<map>
+#include<unordered_map>
 using namespace std;
 using ll = long long;
+
 
 //Some predefined templates
 template <typename T>
 ostream& operator <<(ostream& out, const vector<T>& a) {
   out << "["; bool first = true;
-  for (auto v : a) { out << (first ? "" : ", "); out << v; first = 0;} out << "]";
+  for (auto v : a) { out << (first ? "" : ", "); out << v; first = 0;} 
+  out << "]";
   return out;
 }
 
+template <typename U, typename T, class Cmp>
+ostream& operator <<(ostream& out, const map<U, T, Cmp>& a) {
+  out << "{"; bool first = true;
+  for (auto& p : a) { out << (first ? "" : ", "); out << p.first << ":" << p.second; first = 0;} out << "}";
+  return out;
+}
 
+template <typename U, typename T, class Cmp>
+ostream& operator <<(ostream& out, const unordered_map<U, T, Cmp>& a) {
+  out << "{"; bool first = true;
+  for (auto& p : a) { out << (first ? "" : ", "); out << p.first << ":" << p.second; first = 0;} out << "}";
+  return out;
+}
 
 int main()
 {
