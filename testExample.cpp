af8667e397988e59ca135bb1d06bbe75a3cc87ca
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-03-18 00:44:33 -0400

    Create testExample.cpp

---
+++ testExample.cpp
@@ -0,0 +1,109 @@
+#include "testlib.h"
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <queue>
+#include <list>
+using namespace std;
+using ll = long long;
+
+#define pb push_back
+#define f first
+#define se second
+#define ve vector
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define RFOR(i, a, b) for (int i = a; i >= b; i--)
+
+// Some predefined templates
+template <typename T>
+ostream &operator<<(ostream &out, const vector<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename U, typename T, class Cmp>
+ostream &operator<<(ostream &out, const map<U, T, Cmp> &a)
+{
+  out << "{";
+  bool first = true;
+  for (auto &p : a)
+  {
+    out << (first ? "" : ", ");
+    out << p.first << ":" << p.second;
+    first = 0;
+  }
+  out << "}";
+  return out;
+}
+
+template <typename U, typename T, class Cmp>
+ostream &operator<<(ostream &out, const unordered_map<U, T, Cmp> &a)
+{
+  out << "{";
+  bool first = true;
+  for (auto &p : a)
+  {
+    out << (first ? "" : ", ");
+    out << p.first << ":" << p.second;
+    first = 0;
+  }
+  out << "}";
+  return out;
+}
+
+ll gcd(ll x, ll y)
+{
+  return (y == 0 ? x : gcd(y, x % y));
+}
+ll lcm(ll x, ll y)
+{
+  return x * y / gcd(x, y);
+}
+
+int to_int(string &s)
+{
+  int res = 0;
+  for (char c : s)
+  {
+    res = res * 10 + (c - '0');
+  }
+  return res;
+}
+
+int MOD = 998244353;
+int mod = 998244353;
+int INF = INT_MAX;
+
+int main(int argc, char * argv[])
+{
+    setName("compare two signed int%d's", 8 * int(sizeof(int)));
+    registerTestlibCmd(argc, argv);
+    
+    int x = inf.readInt();
+    int y = ouf.readInt();
+    if(y <= 0) {
+        quitf(_wa, ": expect a positive number");
+    }
+    int g = __gcd(x, y);
+    if (g != 1)
+        quitf(_wa, ": %d and %d are not co-prime", x, y);
+    
+    quitf(_ok, "");
+}
