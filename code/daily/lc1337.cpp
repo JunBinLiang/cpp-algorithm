214479cf2afef12f65b0b2d5982ce827a7a09305
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-03-27 01:09:21 -0400

    Create lc1337.cpp

---
+++ code/daily/lc1337.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <queue>
+#include <list>
+using namespace std;
+using ll = long long;
+
+#define pb push_back
+#define f first
+#define se second
+#define ve vector
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define RFOR(i, a, b) for (int i = a; i >= b; i--)
+#define W while
+#define um unordered_map
+#define us unordered_set
+#define str string
+
+// Some predefined templates
+template <typename T>
+ostream &operator<<(ostream &out, const vector<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename U, typename T, class Cmp>
+ostream &operator<<(ostream &out, const map<U, T, Cmp> &a)
+{
+  out << "{";
+  bool first = true;
+  for (auto &p : a)
+  {
+    out << (first ? "" : ", ");
+    out << p.first << ":" << p.second;
+    first = 0;
+  }
+  out << "}";
+  return out;
+}
+
+template <typename U, typename T, class Cmp>
+ostream &operator<<(ostream &out, const unordered_map<U, T, Cmp> &a)
+{
+  out << "{";
+  bool first = true;
+  for (auto &p : a)
+  {
+    out << (first ? "" : ", ");
+    out << p.first << ":" << p.second;
+    first = 0;
+  }
+  out << "}";
+  return out;
+}
+
+
+ll gcd(ll x, ll y)
+{
+  return (y == 0 ? x : gcd(y, x % y));
+}
+ll lcm(ll x, ll y)
+{
+  return x * y / gcd(x, y);
+}
+
+int to_int(string &s)
+{
+  int res = 0;
+  for (char c : s)
+  {
+    res = res * 10 + (c - '0');
+  }
+  return res;
+}
+
+bool COMP(ve<int>& a, ve<int>& b) {
+  if(a[0] == b[0]) {
+      return a[1] < b[1];
+  }
+  return a[0] < b[0];
+}
+
+
+int MOD = 998244353;
+int mod = 1000000007;
+
+
+
+
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& a, int k) {
+        int n = a.size(), m = a[0].size();
+        ve<ve<int>> b;
+        ve<int> res;
+        FOR(i, 0, n) {
+            int sum = 0;
+            FOR(j, 0, m) {
+                sum += a[i][j];
+            }
+            b.pb({sum, i});
+        }
+        sort(b.begin(), b.end(),COMP);
+        FOR(i, 0, k) {
+            res.pb(b[i][1]);
+        }
+        return res;
+    }
+};
