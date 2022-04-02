242662356c657f4904944d97cc6dbc644f0dc921
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-04-02 12:07:11 -0400

    Create 2220.cpp

---
+++ code/weekly-contest/2220.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <queue>
+#include <list>
+using namespace std;
+using ll = long long;
+
+#define pb push_back
+#define f first
+#define se second
+#define ve vector
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define W while
+#define RFOR(i, a, b) for (int i = a; i >= b; i--)
+#define um unordered_map
+#define us unordered_set
+
+
+template <typename T>
+ostream &operator<<(ostream &out, const vector<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+
+template <typename T>
+ostream &operator<<(ostream &out, const unordered_set<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename U, typename T, class Cmp>
+ostream &operator<<(ostream &out, const map<U, T, Cmp> &a)
+{
+  out << "{";
+  bool first = true;
+  for (auto &p : a)
+  {
+    out << (first ? "" : ", ");
+    out << p.first << ":" << p.second;
+    first = 0;
+  }
+  out << "}";
+  return out;
+}
+
+template <typename U, typename T, class Cmp>
+ostream &operator<<(ostream &out, const unordered_map<U, T, Cmp> &a)
+{
+  out << "{";
+  bool first = true;
+  for (auto &p : a)
+  {
+    out << (first ? "" : ", ");
+    out << p.first << ":" << p.second;
+    first = 0;
+  }
+  out << "}";
+  return out;
+}
+
+
+ll gcd(ll x, ll y)
+{
+  return (y == 0 ? x : gcd(y, x % y));
+}
+ll lcm(ll x, ll y)
+{
+  return x * y / gcd(x, y);
+}
+
+int to_int(string &s)
+{
+  int res = 0;
+  for (char c : s)
+  {
+    res = res * 10 + (c - '0');
+  }
+  return res;
+}
+
+bool COMP(ve<int>& a, ve<int>& b) {
+  return false;
+}
+int MOD = 998244353;
+int mod = 1000000007;
+
+
+
+
+class Solution {
+public:
+    int minBitFlips(int a, int b) {
+        int res = 0;
+        FOR(i, 0, 31) {
+            int b1 = 0, b2 = 0;
+            if(a & (1 << i)) {
+                b1 = 1;
+            }
+            if(b & (1 << i)) {
+                b2 = 1;
+            }
+            if(b1 != b2) res++;
+        }
+        return res;
+    }
+};
