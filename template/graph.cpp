a81d2985f04a9bd1b97f8e08b0b0cb01ac4dd757
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-04-10 04:04:17 -0400

    Create graph.cpp

---
+++ template/graph.cpp
@@ -0,0 +1,17 @@
+const int N = 10010, M = N * 2;
+int h[N], e[M], w[M], ne[M], idx;
+
+void add(int a, int b, int c)
+{
+  e[idx] = b, w[idx] = c, ne[idx] = h[a], h[a] = idx ++ ;
+}
+
+int get_size(int u, int fa)  // 求子树大小
+{
+    if (st[u]) return 0;
+    int res = 1;
+    for (int i = h[u]; ~i; i = ne[i])
+        if (e[i] != fa)
+            res += get_size(e[i], u);
+    return res;
+}
