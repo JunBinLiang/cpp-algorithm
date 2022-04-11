69f9d9db35922f73076deede496607fd7e7d2763
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-04-11 03:24:35 -0400

    Create minSeg.cpp

---
+++ template/minSeg.cpp
@@ -0,0 +1,51 @@
+const int MAX = 200000 + 100;
+
+struct Node {
+  int l, r;
+  int mn;
+} tree[MAX * 4];
+
+void build(int id) {
+  int l = tree[id].l, r = tree[id].r;
+  if(l == r) {
+    return;
+  }
+  int mid = l + (r - l) / 2;
+  tree[id * 2 + 1] = {l, mid, 500000};
+  tree[id * 2 + 2] = {mid + 1, r, 500000};
+  build(id * 2 + 1);
+  build(id * 2 + 2);
+}
+
+void update(int id, int index, ll val) { 
+  int l = tree[id].l, r = tree[id].r;
+  if(l == r) {
+    tree[id].mn = val; 
+    return;
+  }
+
+  int mid = l + (r - l) / 2;
+  int left = id * 2 + 1, right = id * 2 + 2;
+  if(index <= mid) {
+    update(left, index, val);
+  } else {
+    update(right, index, val);
+  }
+  tree[id].mn = min(tree[left].mn, tree[right].mn);
+}
+
+int query(int id, int s, int e) {
+  int l = tree[id].l, r = tree[id].r;
+  if(l == s && r == e){
+    return tree[id].mn;
+  }
+  int mid = l + (r - l) / 2;
+  int left = id * 2 + 1, right = id * 2 + 2;
+  if(e <= mid){
+      return query(left, s, e);
+  } else if(s >= mid + 1) {
+      return query(right, s, e);
+  } else {
+    return min(query(left, s, mid), query(right, mid + 1, e));
+  }
+}
