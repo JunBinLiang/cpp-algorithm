89d881b83be84098e0dbe64605d377fca5e3a1e2
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-04-15 15:34:30 -0400

    Update array.cpp

--- introduction/array.cpp
+++ introduction/array.cpp
@@ -12,3 +12,23 @@ int main() {
     cout << a[0] << " " << a[1] <<" " << a[2] << endl;
     return 0;
 }
+
+
+
+/*
+#include<iostream> 
+#include<string> 
+using namespace std;
+
+//array
+int main() {
+  int a[5];
+  for(int i = 0; i < 5; i++) {
+      cin >> a[i];
+  }
+  for(int i = 0; i < 5; i++) {
+      cout << a[i] << endl;
+  }
+}
+
+*/
