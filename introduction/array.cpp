f0cb21053f49b5b97e07da395c58dce2c120ccb2
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-04-15 15:33:13 -0400

    Create array.cpp

---
+++ introduction/array.cpp
@@ -0,0 +1,14 @@
+#include<iostream> 
+#include<string> 
+using namespace std;
+
+//array
+
+int main() {
+    string a[10]; 
+    a[0] = "hi";
+    a[1] = "yes";
+    a[2] = "no";
+    cout << a[0] << " " << a[1] <<" " << a[2] << endl;
+    return 0;
+}
