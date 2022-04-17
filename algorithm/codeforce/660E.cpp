1e0e9bd8fad0dd425a631eb9fc7f354e19149e3e
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-04-17 17:12:38 -0400

    Create 660E.cpp

---
+++ algorithm/codeforce/660E.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <queue>
+#include <list>
+
+using namespace std;
+using ll = long long;
+#define pb push_back
+#define ve vector
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define RFOR(i, a, b) for (int i = a; i >= b; i--)
+#define f first
+#define se second
+#define W while
+#define um unordered_map
+#define us unordered_set
+#define be begin
+#define en end
+
+// Some predefined templates
+template <typename T>
+ostream &operator<<(ostream &out, const vector<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename T>
+ostream &operator<<(ostream &out, const unordered_set<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename T>
+ostream &operator<<(ostream &out, const set<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename U, typename T, class Cmp>
+ostream &operator<<(ostream &out, const map<U, T, Cmp> &a)
+{
+  out << "{";
+  bool first = true;
+  for (auto &p : a)
+  {
+    out << (first ? "" : ", ");
+    out << p.first << ":" << p.second;
+    first = 0;
+  }
+  out << "}";
+  return out;
+}
+
+template <typename U, typename T, class Cmp>
+ostream &operator<<(ostream &out, const unordered_map<U, T, Cmp> &a)
+{
+  out << "{";
+  bool first = true;
+  for (auto &p : a)
+  {
+    out << (first ? "" : ", ");
+    out << p.first << ":" << p.second;
+    first = 0;
+  }
+  out << "}";
+  return out;
+}
+
+
+ll gcd(ll x, ll y)
+{
+  return (y == 0 ? x : gcd(y, x % y));
+}
+ll lcm(ll x, ll y)
+{
+  return x * y / gcd(x, y);
+}
+
+int to_int(string &s)
+{
+  int res = 0;
+  for (char c : s)
+  {
+    res = res * 10 + (c - '0');
+  }
+  return res;
+}
+
+bool COMP(ve<int>& a, ve<int>& b) {
+  return a[0] < b[0];
+}
+
+/*class Compare
+{
+public:
+    bool operator() (ve<int>& a, ve<int>& b)
+    {
+        return a[0] > b[0];
+    }
+};*/
+
+
+int mod = 1000000007;
+ll dp[1000000 + 50];
+ll sum[1000000 + 50];
+void solve() {
+  int n, m;
+  scanf("%d%d", &n, &m);
+  memset(dp, 0, sizeof(dp));
+  memset(sum, 0, sizeof(sum));
+
+  dp[0] = 1;
+  for(int i = 1; i <= n; i++) {
+    dp[i] = dp[i - 1] * 2 - sum[i - 1];
+    dp[i] += mod;
+    dp[i] %= mod;
+    dp[i] *= m;
+    dp[i] %= mod;
+
+    sum[i] = sum[i - 1] * (m - 1) + dp[i - 1];
+    sum[i] %= mod;
+  }
+
+  cout << dp[n] << endl;
+
+
+
+}
+
+
+
+int main()
+{
+  int t = 1;
+  //cin >> t;
+  W (t--)
+  {
+    solve();
+  }
+  return 0;
+}
