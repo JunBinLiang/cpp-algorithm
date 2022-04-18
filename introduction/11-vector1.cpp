9e9caa0658ff4ab8d733deb1ec0ee52405682aae
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-04-18 15:54:36 -0400

    Create 11-vector1.cpp

---
+++ introduction/11-vector1.cpp
@@ -0,0 +1,21 @@
+#include<iostream> 
+#include<vector> 
+using namespace std;
+
+
+//array : fix size
+//vercor : (object) array, dynamic size
+
+int main() {
+    vector<int> a(2);
+    a[0] = 1;
+    a[1] = 2;
+    cout << a.size() << endl;
+    a.push_back(10);
+    cout << a.size() << endl;
+    cout << endl;
+    for(int i = 0; i < a.size(); i++) {
+        cout << a[i] << endl;
+    }
+    return 0;
+}
