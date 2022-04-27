13bb50e3948c5425df5fd1b6bb7e2b4a99b73136
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-04-27 10:14:26 -0400

    Update combination.cpp

--- template/combination.cpp
+++ template/combination.cpp
@@ -3,29 +3,30 @@ namespace Combination {
   ll fact[1000000 + 100];
   ll inv[1000000 + 100];
   int c[505][505];
-  ll modInverse(ll a, ll m) {
-      ll m0 = m;
-      ll y = 0, x = 1;
-      if (m == 1)
-          return 0;
-      while (a > 1) {
+ll modInverse(ll a, ll m) {
+    ll m0 = m;
+    ll y = 0, x = 1;
+    if (m == 1)
+        return 0;
+    while (a > 1) {
           // q is quotient
-          ll q = a / m;
-          ll t = m;
-          // m is remainder now, process
-          // same as Euclid's algo
-          m = a % m;
-          a = t;
-          t = y;
-          // Update x and y
-          y = x - q * y;
-          x = t;
-      }
-      // Make x positive
-      if (x < 0)
-          x += m0;
-      return x;
-  }
+        ll q = a / m;
+        ll t = m;
+        // m is remainder now, process
+        // same as Euclid's algo
+        m = a % m;
+        a = t;
+        t = y;
+        // Update x and y
+        y = x - q * y;
+        x = t;
+    }
+    // Make x positive
+    if (x < 0)
+        x += m0;
+    return x;
+}
+
 
 
 
