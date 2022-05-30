15e9a4b03ca9d6403700cc857b089820e0240649
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-05-30 15:47:44 -0400

    Create 877C.cpp

---
+++ constructive/877C.cpp
@@ -0,0 +1,195 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <queue>
+#include <list>
+
+using namespace std;
+using ll = long long;
+#define pb push_back
+#define ve vector
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define RFOR(i, a, b) for (int i = a; i >= b; i--)
+#define f first
+#define se second
+#define W while
+#define um unordered_map
+#define us unordered_set
+#define be begin
+#define en end
+
+// Some predefined templates
+template <typename T>
+ostream &operator<<(ostream &out, const vector<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename T>
+ostream &operator<<(ostream &out, const unordered_set<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename T>
+ostream &operator<<(ostream &out, const set<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename U, typename T, class Cmp>
+ostream &operator<<(ostream &out, const map<U, T, Cmp> &a)
+{
+  out << "{";
+  bool first = true;
+  for (auto &p : a)
+  {
+    out << (first ? "" : ", ");
+    out << p.first << ":" << p.second;
+    first = 0;
+  }
+  out << "}";
+  return out;
+}
+
+template <typename U, typename T, class Cmp>
+ostream &operator<<(ostream &out, const unordered_map<U, T, Cmp> &a)
+{
+  out << "{";
+  bool first = true;
+  for (auto &p : a)
+  {
+    out << (first ? "" : ", ");
+    out << p.first << ":" << p.second;
+    first = 0;
+  }
+  out << "}";
+  return out;
+}
+
+
+ll gcd(ll x, ll y)
+{
+  return (y == 0 ? x : gcd(y, x % y));
+}
+ll lcm(ll x, ll y)
+{
+  return x * y / gcd(x, y);
+}
+
+int to_int(string &s)
+{
+  int res = 0;
+  for (char c : s)
+  {
+    res = res * 10 + (c - '0');
+  }
+  return res;
+}
+
+string substring(string& s, int l, int r) {
+  string res = "";
+  for(int i = l; i <= r; i++) res += s[i];
+  return res;
+}
+
+/*class Compare
+{
+public:
+    bool operator() (ve<ll>& a, ve<ll>& b)
+    {
+        return a[0] > b[0];
+    }
+    //a[0] > b[0] : min
+};*/
+
+bool COMP(const ve<int>& a, const ve<int>& b) {
+  return a[0] < b[0];
+}
+
+
+
+void solve() {
+  int n;
+  scanf("%d", &n);
+  ve<int> res;
+
+  if(n % 2 == 0) {
+    for(int i = 1; i <= n; i += 2) {
+      res.pb(i);
+    }
+    for(int i = 2; i <= n; i += 2) {
+      res.pb(i);
+    }
+    for(int i = 1; i <= n; i += 2) {
+      res.pb(i);
+    }
+  } else {
+    for(int i = 2; i <= n; i += 2) {
+      res.pb(i);
+    }
+    for(int i = 1; i <= n; i += 2) {
+      res.pb(i);
+    }
+    for(int i = 2; i <= n; i += 2) {
+      res.pb(i);
+    }
+  }
+
+
+  cout << res.size() << endl;
+  for(int i : res) {
+    cout << i << " ";
+  }
+  cout << endl;
+
+
+}
+
+int main()
+{
+
+  int t = 1;
+  //cin >> t;
+  W (t--)
+  {
+    solve();
+  }
+  return 0;
+}
+
+//炸偶数，所有偶数都会到奇数位置。反之亦然
