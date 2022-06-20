1f15ab6aa88178973fb4bacaec25414f25d5d936
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-06-20 00:20:35 -0400

    Create 1088E.cpp

---
+++ algorithm/codeforce/1088E.cpp
@@ -0,0 +1,210 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <queue>
+#include <list>
+
+using namespace std;
+using ll = long long;
+#define pb push_back
+#define ve vector
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define RFOR(i, a, b) for (int i = a; i >= b; i--)
+#define f first
+#define se second
+#define W while
+#define um unordered_map
+#define us unordered_set
+#define be begin
+#define en end
+
+// Some predefined templates
+template <typename T>
+ostream &operator<<(ostream &out, const vector<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename T>
+ostream &operator<<(ostream &out, const unordered_set<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename T>
+ostream &operator<<(ostream &out, const set<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename U, typename T, class Cmp>
+ostream &operator<<(ostream &out, const map<U, T, Cmp> &a)
+{
+  out << "{";
+  bool first = true;
+  for (auto &p : a)
+  {
+    out << (first ? "" : ", ");
+    out << p.first << ":" << p.second;
+    first = 0;
+  }
+  out << "}";
+  return out;
+}
+
+template <typename U, typename T, class Cmp>
+ostream &operator<<(ostream &out, const unordered_map<U, T, Cmp> &a)
+{
+  out << "{";
+  bool first = true;
+  for (auto &p : a)
+  {
+    out << (first ? "" : ", ");
+    out << p.first << ":" << p.second;
+    first = 0;
+  }
+  out << "}";
+  return out;
+}
+
+
+ll gcd(ll x, ll y)
+{
+  return (y == 0 ? x : gcd(y, x % y));
+}
+ll lcm(ll x, ll y)
+{
+  return x * y / gcd(x, y);
+}
+
+int to_int(string &s)
+{
+  int res = 0;
+  for (char c : s)
+  {
+    res = res * 10 + (c - '0');
+  }
+  return res;
+}
+
+string substring(string& s, int l, int r) {
+  string res = "";
+  for(int i = l; i <= r; i++) res += s[i];
+  return res;
+}
+
+/*class Compare
+{
+public:
+    bool operator() (pair<int, int>& a, pair<int, int>& b)
+    {
+        return a.first > b.first;
+    }
+    //a[0] > b[0] : min
+};*/
+
+bool COMP(const pair<int, int>&p1, const pair<int, int>&p2) {
+  return p1.second < p2.second;
+}
+
+
+int a[300000 + 5];
+ll dp[300000 + 5];
+ll res = -1000000000;
+ll k = 0;
+void dfs(ve<ve<int>>&graph, int pa, int root) {
+  dp[root] = a[root];
+  for(int &nxt : graph[root]) {
+    if(nxt == pa) continue;
+    dfs(graph, root, nxt);
+    if(dp[nxt] > 0) {
+      dp[root] += dp[nxt];
+    }
+  }
+  res = max(res,dp[root]);
+}
+
+void dfs1(ve<ve<int>>&graph, int pa, int root) {
+  dp[root] = a[root];
+  for(int &nxt : graph[root]) {
+    if(nxt == pa) continue;
+    dfs1(graph, root, nxt);
+    if(dp[nxt] > 0) {
+      dp[root] += dp[nxt];
+    }
+  }
+  //cout <<"go " << root + 1 << " " << dp[root] << endl;
+  if(dp[root] == res) {
+      k++;
+      dp[root] = 0;
+  }
+}
+
+void solve() {
+  int n;
+  scanf("%d", &n);
+  for(int i = 0; i < n; i++) {
+    scanf("%d", &a[i]);
+  }
+  memset(dp, 0, sizeof(dp));
+  ve<ve<int>> graph(n);
+  for(int i = 0; i < n - 1; i++) {
+    int u, v;
+    scanf("%d%d", &u, &v);
+    u--; v--;
+    graph[u].pb(v);
+    graph[v].pb(u);
+  }
+  dfs(graph, -1, 0);
+  memset(dp, 0, sizeof(dp));
+  dfs1(graph, -1, 0);
+  cout << (res * k) << " " << k << endl;
+
+}
+
+int main()
+{
+
+
+  int t = 1;
+  //cin >> t;
+  W (t--)
+  {
+    solve();
+  }
+  return 0;
+}
