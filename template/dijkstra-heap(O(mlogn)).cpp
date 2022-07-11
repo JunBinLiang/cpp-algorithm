b8f7f713e20aa0a6b32a89ff1b00b8d98d11b34f
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-07-10 23:32:19 -0400

    Create dijkstra-heap(O(mlogn)).cpp

---
+++ template/dijkstra-heap(O(mlogn)).cpp
@@ -0,0 +1,28 @@
+void dijkstra()  // 求1号点到n号点的最短路距离
+{
+    memset(dist, 0x3f, sizeof dist);
+    dist[1] = 0;
+    priority_queue<PII, vector<PII>, greater<PII>> heap;
+    heap.push({0, 1});
+
+    while (heap.size())
+    {
+        auto t = heap.top();
+        heap.pop();
+
+        int ver = t.second, distance = t.first;
+
+        if (st[ver]) continue;
+        st[ver] = true;
+
+        for (int i = h[ver]; i != -1; i = ne[i])
+        {
+            int j = e[i];
+            if (dist[j] > dist[ver] + w[i])
+            {
+                dist[j] = dist[ver] + w[i];
+                heap.push({dist[j], j});
+            }
+        }
+    }
+}
