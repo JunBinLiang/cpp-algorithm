f361b9e8e4b390a5a9685d367437e9e253533fb6
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-07-30 20:20:47 -0400

    Create 有向图强连通.cpp

---
+++ template/有向图强连通.cpp
@@ -0,0 +1,41 @@
+const int N = 10010, M = 50010;
+int h[N], e[M], ne[M], idx;
+
+int dfn[N], low[N], timestamp;
+int stk[N], top;
+bool in_stk[N];
+
+int id[N], scc_cnt, Size[N];
+
+void add(int a, int b)
+{
+    e[idx] = b, ne[idx] = h[a], h[a] = idx ++ ;
+}
+
+void tarjan(int u)
+{
+    dfn[u] = low[u] = ++ timestamp;
+    stk[ ++ top] = u, in_stk[u] = true;
+    for (int i = h[u]; i != -1; i = ne[i])
+    {
+        int j = e[i];
+        if (!dfn[j])
+        {
+            tarjan(j);
+            low[u] = min(low[u], low[j]);
+        }
+        else if (in_stk[j]) low[u] = min(low[u], dfn[j]);
+    }
+
+    if (dfn[u] == low[u])
+    {
+        ++ scc_cnt;
+        int y;
+        do {
+            y = stk[top -- ];
+            in_stk[y] = false;
+            id[y] = scc_cnt;
+            Size[scc_cnt] ++ ;
+        } while (y != u);
+    }
+}
