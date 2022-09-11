6deff8c9d14e8b8c88800ce638027c91f204d4c4
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-09-11 10:27:46 -0400

    Update and rename kmp.java to kmp.cpp

---
+++ template/kmp.cpp
@@ -0,0 +1,17 @@
+vector<int> pre(string& pattern){
+  vector<int> pre(pattern.size());
+  int l=0,r=1;
+  while(r < pattern.size()()){
+    if(pattern[r] == pattern[l]){
+      pre[r] = l + 1;
+      l++;
+      r++;
+    }else{
+      if(l == 0)r++;
+      else{
+            l=pre[l-1];
+          }
+      }
+    }
+    return pre;
+}
