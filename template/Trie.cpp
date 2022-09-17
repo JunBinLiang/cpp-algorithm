b3283a65ee3c0220ad5e86952621971dd67dcaa6
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-09-17 13:50:41 -0400

    Create Trie.cpp

---
+++ template/Trie.cpp
@@ -0,0 +1,39 @@
+const int N = 1e6;
+struct Node {
+    int nxt[26];
+} nodes[N];
+int idx = 2;
+
+class Trie {
+public:
+    set<string> s;
+    Trie() {
+        nodes[1] = {};
+    }
+    
+    void insert(string word) {
+        s.insert(word);
+        int root = 1;
+        for(char c : word) {
+            int nxt = c - 'a';
+            if(nodes[root].nxt[nxt] == 0) {
+                nodes[idx] = {};
+                nodes[root].nxt[nxt] = idx;
+                idx++;
+            } 
+            root = nodes[root].nxt[nxt];
+        }
+    }
+        
+    bool startsWith(string prefix) {
+        int root = 1;
+        for(char c : prefix) {
+            int nxt = c - 'a';
+            if(nodes[root].nxt[nxt] == 0) {
+                return false;
+            } 
+            root = nodes[root].nxt[nxt];
+        }
+        return true;
+    }
+};
