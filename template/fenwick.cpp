fde5e78f8c41d9202bb5ac8a4309ee8bd175c964
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-02-27 03:21:37 -0500

    Update fenwick.cpp

--- template/fenwick.cpp
+++ template/fenwick.cpp
@@ -1,8 +1,8 @@
-namespace Fenwick {
-  const int SIZE = 200000 + 100;
+const int SIZE = 200000 + 100;
+struct Fenwick {
   int tree[SIZE];
-  void init() {
-    memset(tree, 0, sizeof(tree));
+  void init(int n) {
+    for(int i = 0; i <= n + 1; i++) tree[i] = 0;
   }
 
   void update(int i, int val) {
@@ -24,45 +24,4 @@ namespace Fenwick {
   int query(int i, int j) {
       return pre(j + 1) - pre(i);
   }
-}
-
-
-
-
-
-
-
-
-
-//multiple fenwick tree
-namespace Fenwick {
-  const int SIZE = 100000 + 100;
-  int tree[2][SIZE];
-  void init(int n = 100000 + 100) {
-    SIZE = n + 5;
-    FOR(i, 0, n + 5) {
-      tree[0][i] = 0;
-      tree[1][i] = 0;
-    }
-  }
-
-  void update(int which, int i, int val) {
-    i++;
-    while(i < SIZE){
-      tree[which][i] += val;
-      i += (i & -i);
-    }
-  }
-
-  int pre(int which, int i) {
-    int sum = 0;
-    while(i > 0){
-      sum += tree[which][i];
-      i -= ( i & -i);
-    }
-    return sum;
-  }
-  int query(int which, int i, int j) {
-      return pre(which, j + 1) - pre(which, i);
-  }
-}
+} tr1;
